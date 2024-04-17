a4fb9a91467d3efe4ca2745108328ba1d09cccb6
neeraj-2002 <neerajninawe1@gmail.com>
2024-04-17 17:54:48 +0530

    add prod route

--- frontend/src/pages/ChngPassword/ChngPassword.js
+++ frontend/src/pages/ChngPassword/ChngPassword.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { toast } from "react-toastify";
 
 function ChngPassword() {
